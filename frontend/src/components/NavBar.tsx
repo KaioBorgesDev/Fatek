835340ac9a9581d8b2928184fbb9f78b0e52a164
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-04 14:00:59 -0300

    fix + feat: menu hamburguer fixed + menu for litlle cart kkkkkkkkkkkk

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
+import { MdShoppingCartCheckout } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 const NavBar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const [cartActive, setCartActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
+  const toggleCart = () => {
+    setCartActive(!cartActive);
+  };
+
   return (
     <>
       <nav>
         <div className="logo">
-          <h1>FATEK</h1>
+          <a href="">
+            <h1>FATEK</h1>
+          </a>
         </div>
         <ul>
           <li>
@@ -22,27 +37,47 @@ const NavBar = () => {
             <a href="#ancora-contatos">Contatos</a>
           </li>
         </ul>
-        <div className="hamburger">
+        <div
+          className={`hamburger ${menuActive ? "hamburger-active" : ""}`}
+          onClick={toggleMenu}
+        >
           <span className="line"></span>
           <span className="line"></span>
           <span className="line"></span>
+        </div>
+        <div className="cart-icon" onClick={toggleCart}>
+          <MdShoppingCartCheckout size={24} />
         </div>
       </nav>
-      <div className="menubar">
+      {/* Menu Hamburguer */}
+      <div className={`menubar ${menuActive ? "active" : ""}`}>
         <ul>
           <li>
-            <a href="#ancora-home">Home</a>
+            <a href="#ancora-home" onClick={toggleMenu}>Home</a>
           </li>
           <li>
-            <a href="#ancora-sobre-mim">Sobre Mim</a>
+            <a href="#ancora-sobre-mim" onClick={toggleMenu}>Sobre Mim</a>
           </li>
           <li>
-            <a href="#ancora-projetos">Projetos</a>
+            <a href="#ancora-projetos" onClick={toggleMenu}>Projetos</a>
           </li>
           <li>
-            <a href="#ancora-contatos">Contatos</a>
+            <a href="#ancora-contatos" onClick={toggleMenu}>Contatos</a>
           </li>
         </ul>
+      </div>
+      {/* Cart Card */}
+      <div className={`cart-card ${cartActive ? "active" : ""}`}>
+        <div className="card-head">
+        <h2>Meu Carrinho</h2>
+        <button onClick={toggleCart} className="close-cart"><IoClose size={20}/></button>
+        </div>
+        <ul>
+          <li>Produto 1 - R$100,00</li>
+          <li>Produto 2 - R$50,00</li>
+          <li>Produto 3 - R$30,00</li>
+        </ul>
+        
       </div>
     </>
   );
