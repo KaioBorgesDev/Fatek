dc67764c4102a6d7356622a806b56ab1082eba13
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 19:22:24 -0300

    feat- jwt token implements added on project

---
+++ backend/src/infra/Jwt/JwtTokenService.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { ITokenService } from "src/adapters/services/ITokenService";
+
+export class JwtTokenService implements ITokenService {
+  private secretKey: string;
+
+  constructor(secretKey: string) {
+    this.secretKey = secretKey;
+  }
+
+  generateToken(payload: object): string {
+    return jwt.sign(payload, this.secretKey, { expiresIn: '1h' });
+  }
+
+  verifyToken(token: string): object | null {
+    try {
+      return jwt.verify(token, this.secretKey);
+    } catch (error) {
+      return null;
+    }
+  }
+}
