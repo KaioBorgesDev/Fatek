72a7c28e47f6d2167d24fbcacce6040715fd5b0f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 20:39:03 -0300

    feat- register controller added

--- backend/src/adapters/controllers/UserController.ts
+++ backend/src/adapters/controllers/UserController.ts
@@ -1,19 +1,42 @@
+import User from "src/entities/User";
 import { JwtTokenService } from "src/infra/Service/JwtTokenService";
-import UserRepositoryImp from "src/infra/Service/UserRepositoryImp"
-import LoginUseCase from "src/usecases/UserCases/LoginUseCase"
+import UserRepositoryImp from "src/infra/Service/UserRepositoryImp";
+import LoginUseCase from "src/usecases/UserCases/LoginUseCase";
+import RegisterUseCase from "src/usecases/UserCases/RegisterUseCase";
 
-const LoginController = async (req,res) =>{
-    const loginUseCase = new LoginUseCase(new UserRepositoryImp(), new JwtTokenService('SAD'));
+const secretKey = 'SAD';
 
-    try {
-        const jwtoken = await loginUseCase.execute(req.body.email, req.body.password);
 
-        res.status(200).json({token: jwtoken})
-    } catch (error) {
-        res.status(401).json({Erro: "Erro ao fazer o login " + error});
-    }
+const loginController = async (req, res) => {
+  const loginUseCase = new LoginUseCase(new UserRepositoryImp(), new JwtTokenService(secretKey));
+
+  try {
+    const jwtoken = await loginUseCase.execute(req.body.email, req.body.password);
+    res.status(200).json({ token: jwtoken });
+  } catch (error) {
+    res.status(401).json({ Erro: "Erro ao fazer o login: " + error.message });
+  }
+};
+
+
+const registerController = async (req, res) => {
+  const registerUseCase = new RegisterUseCase(new UserRepositoryImp);
+  const { email, password } = req.body;
+  
+  try {
+    const user = new User(email, password);
     
-}
+    await registerUseCase.execute(email, user.passwordHash);
+
+    res.status(201).json({ message: "Usuário registrado com sucesso" });
+  } catch (error) {
+    res.status(400).json({ Erro: "Erro ao registrar usuário: " + error.message });
+  }
+};
 
 
-module.exports = LoginController
+module.exports = {
+  loginController,
+  registerController,
+  
+};
