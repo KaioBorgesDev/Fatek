80b0691c6d5ed9c34b27bc37ba3221f264a50344
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 19:44:14 -0300

    fix- folder reajusted

--- backend/src/infra/Service/UserRepositoryImp.ts
+++ backend/src/infra/Service/UserRepositoryImp.ts
@@ -1,12 +1,15 @@
 import UserRepository from "src/adapters/repository/UserRepository";
 import User from "src/entities/User";
+import UserModel from "../Model/UserModel";
+
 
 export default class UserRepositoryImp implements UserRepository{
     findByEmail(email: string): Promise<User> {
         throw new Error("Method not implemented.");
     }
     async save(email: string, password: string): Promise<void> {
-        
+        const user = User.create(email, password);
+        await UserModel.create(user);
     }
 
 }
