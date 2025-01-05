cafab9541502246a584c6e708fb7ce9347f9715b
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 13:41:44 -0300

    feat- new images add

--- frontend/src/components/SectionProduct/SectionProduct.tsx
+++ frontend/src/components/SectionProduct/SectionProduct.tsx
@@ -6,14 +6,14 @@ const SectionProduct = () => {
     <div className="container">
       <div className="cards"> 
         <div className="card-image-principal">
-            <img src="/assets/book-1.jpg" alt="Principal" />
+            <img src="/assets/book-6.jpg" alt="Principal" />
         </div>
         <div className="card-column">
           <div className="card-image">
-            <img src="/assets/book-2.jpg" alt="Secondary 1" />
+            <img src="/assets/book-7.jpg" alt="Secondary 1" />
           </div>
           <div className="card-image">
-            <img src="/assets/book-5.jpg" alt="Secondary 2" />
+            <img src="/assets/book-8.jpg" alt="Secondary 2" />
           </div>
         </div>
       </div>
