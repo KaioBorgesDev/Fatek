1857f8758a582e2b565cedb68018c62c334a7037
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 14:55:18 -0300

    fix- section product with date dynamic

--- frontend/src/components/SectionProduct/SectionProduct.tsx
+++ frontend/src/components/SectionProduct/SectionProduct.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React from 'react'; 
 import './SectionProduct.css';
+import PriceCard from '../PriceCard/PriceCard';
+
+type Product = {
+  src: string;
+  alt: string;
+  price: number 
+};
+
+const products: Product[] = [
+  { src: "/assets/book-6.jpg", alt: "Principal",  price: 19.90},
+  { src: "/assets/book-7.jpg", alt: "Secondary 1",  price: 29.90},
+  { src: "/assets/book-8.jpg", alt: "Secondary 2",  price: 9.90},
+  // Adicione outros produtos aqui
+];
 
 const SectionProduct = () => {
   return (
     <div className="container">
-      <div className="cards"> 
+      <div className="cards">
+        {/* Card Principal */}
         <div className="card-image-principal">
-            <img src="/assets/book-6.jpg" alt="Principal" />
+          <img src={products[0].src} alt={products[0].alt} />
+          <PriceCard price={products[0].price}></PriceCard>
         </div>
+
+        {/* Card Secundários */}
         <div className="card-column">
-          <div className="card-image">
-            <img src="/assets/book-7.jpg" alt="Secondary 1" />
-          </div>
-          <div className="card-image">
-            <img src="/assets/book-8.jpg" alt="Secondary 2" />
-          </div>
+          {products.slice(1).map((product, index) => (
+            <div className="card-image" key={index}>
+              <img src={product.src} alt={product.alt} />
+              <PriceCard price={product.price}></PriceCard>
+            </div>
+          ))}
         </div>
       </div>
     </div>
