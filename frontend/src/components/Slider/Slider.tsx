eb991cc64d249eb7adf17e813f987114e3ded47f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 14:22:17 -0300

    fix- slider with box-shadow and gap smaller

--- frontend/src/components/Slider/Slider.tsx
+++ frontend/src/components/Slider/Slider.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import './Slider.css';
+import PriceCard from '../PriceCard/PriceCard';
 
-const Slider = () => {
+
+interface Image {
+  src: string;
+  alt: string;
+  price: string;
+}
+
+const images: Image[] = [
+  { src: '/assets/book-4.jpg', alt: 'Livro 1', price: 'R$ 19,90' },
+  { src: '/assets/book-8.jpg', alt: 'Livro 2', price: 'R$ 29,90' },
+  { src: '/assets/book-9.jpg', alt: 'Livro 3', price: 'R$ 10,90' },
+  { src: '/assets/book-1.jpg', alt: 'Livro 4', price: 'R$ 10,90' },
+  { src: '/assets/book-2.jpg', alt: 'Livro 5', price: 'R$ 10,90' },
+  { src: '/assets/book-6.jpg', alt: 'Livro 6', price: 'R$ 10,90' },
+];
+
+const Slider: React.FC = () => {
   return (
     <div className="slider-container">
       <div className="slider">
-        <div className="slide">
-          <img src="/assets/book-1.jpg" alt="Image 1" />
-        </div>
-        <div className="slide">
-          <img src="/assets/book-2.jpg" alt="Image 2" />
-        </div>
-        <div className="slide">
-          <img src="/assets/book-3.jpg" alt="Image 3" />
-        </div>
-        <div className="slide">
-          <img src="/assets/book-4.jpg" alt="Image 3" />
-        </div>
-        <div className="slide">
-          <img src="/assets/book-5.jpg" alt="Image 3" />
-        </div>
-       
-      </div>
-      <div className="slider-scroll">
-        <div className="scroll-bar"></div>
+        {images.map((image, index) => (
+          <div className="slide" key={index}>
+            <img src={image.src} alt={image.alt} className="slide-image" />
+            <PriceCard price={image.price} />
+          </div>
+        ))}
       </div>
     </div>
   );
