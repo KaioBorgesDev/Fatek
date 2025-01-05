8493401432b67bdd4a7b61afc6d09b28a526083f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 14:56:10 -0300

    feat- imagetype added and slider using now

--- frontend/src/components/Slider/Slider.tsx
+++ frontend/src/components/Slider/Slider.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './Slider.css';
 import PriceCard from '../PriceCard/PriceCard';
+import { ImageType } from '../../types/ImageType';
 
 
-interface Image {
-  src: string;
-  alt: string;
-  price: string;
-}
-
-const images: Image[] = [
-  { src: '/assets/book-4.jpg', alt: 'Livro 1', price: 'R$ 19,90' },
-  { src: '/assets/book-8.jpg', alt: 'Livro 2', price: 'R$ 29,90' },
-  { src: '/assets/book-9.jpg', alt: 'Livro 3', price: 'R$ 10,90' },
-  { src: '/assets/book-1.jpg', alt: 'Livro 4', price: 'R$ 10,90' },
-  { src: '/assets/book-2.jpg', alt: 'Livro 5', price: 'R$ 10,90' },
-  { src: '/assets/book-6.jpg', alt: 'Livro 6', price: 'R$ 10,90' },
+const images: ImageType[] = [
+  { src: '/assets/book-4.jpg', alt: 'Livro 1', price: 19.90 },
+  { src: '/assets/book-8.jpg', alt: 'Livro 2', price: 29.90 },
+  { src: '/assets/book-9.jpg', alt: 'Livro 3', price: 10.90 },
+  { src: '/assets/book-1.jpg', alt: 'Livro 4', price: 10.90 },
+  { src: '/assets/book-2.jpg', alt: 'Livro 5', price: 10.90 },
+  { src: '/assets/book-6.jpg', alt: 'Livro 6', price: 10.90 },
 ];
 
 const Slider: React.FC = () => {
