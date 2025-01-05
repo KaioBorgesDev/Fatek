d92a0b00acef7d543e504293218bb76a91358886
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-05 14:54:58 -0300

    fix- price fixed for decimal

--- frontend/src/components/PriceCard/PriceCard.tsx
+++ frontend/src/components/PriceCard/PriceCard.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './PriceCards.css';
 
 interface PriceCardProps {
-    price: string; // Define o tipo da propriedade "price" como string
-  }
+    price: number; // Define o tipo da propriedade "price" como string
+}
 
   const PriceCard: React.FC<PriceCardProps> = ({ price }) => {
   return (
     <div className="price-card">
-      {price}
+      R${price.toFixed(2)}
     </div>
   );
 };
