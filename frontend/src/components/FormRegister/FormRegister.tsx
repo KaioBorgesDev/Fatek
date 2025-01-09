723816a59fc12706e7c543850f1fbd058f9909bf
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-09 15:10:18 -0300

    feat- Form Register working

--- frontend/src/components/FormRegister/FormRegister.tsx
+++ frontend/src/components/FormRegister/FormRegister.tsx
@@ -1,58 +1,111 @@
 import { useState } from 'react';
 import './FormRegister.css';
-import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import { useMessage } from '../../context/MessageContext';
 
-const FormLogin = () => {
+interface RegisterPayload {
+  email: string;
+  password: string;
+}
+
+const FormRegister = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const {setMessage} = useMessage()
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault(); // Previne o comportamento padrão do formulário (recarregar a página)
 
-  const handleLogin = () => {
-    // Lógica de autenticação pode ser adicionada aqui
-    console.log('Login:', username, password);
+    const payload: RegisterPayload = {
+      email: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:5002/register", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (response.ok){
+          setMessage("Usuário cadastrado com sucesso!");
+          return navigate("/login")
+      }
+        
+
+      if (response.status === 409) {
+        return toast.error("Este email já está em uso.");
+      }
+      toast.error("Não foi possível cadastrar no sistema.");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      toast.error("Cadastro falhou, verifique a conexão.");
+    }
   };
 
   return (
-    <div className="background">
-      <div className="shape"></div>
-      <div className="shape"></div>
-      <form>
-        <h3>Register</h3>
+    <div>
+      <ToastContainer position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark" />
 
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          placeholder="Email or Phone"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+        <form onSubmit={handleLogin}>
+          <h3>Register</h3>
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label htmlFor="username">Username</label>
+          <input
+            type="email"
+            placeholder="Email"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
 
-        <button type="button" onClick={handleLogin}>
-          Register
-        </button>
-        <div className="social">
-          <div className="go">
-            <i className="fab fa-google"></i> Google
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <input type="submit" value="Register" className='button'/>
+          
+          <div className="social">
+            <div className="go">
+              <i className="fab fa-google"></i> Google
+            </div>
+            <div className="fb">
+              <i className="fab fa-facebook"></i> Facebook
+            </div>
           </div>
-          <div className="fb">
-            <i className="fab fa-facebook"></i> Facebook
-          </div>
-        </div>
-        <div style={{textAlign: 'center', paddingTop: '10px'}}>
+
+          <div style={{ textAlign: 'center', paddingTop: '10px' }}>
             <Link to={'/login'}>Já possui uma conta? </Link>
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default FormLogin;
+export default FormRegister;
