921a46d65e580a07c892fdf69d81d0dde2ecf043
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-09 15:10:44 -0300

    feat- toast when login is sucessful

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,11 +1,34 @@
+import { useEffect } from 'react';
 import SectionProduct from '../components/SectionProduct/SectionProduct';
 import Slider from '../components/Slider/Slider';
+import { useMessage } from '../context/MessageContext';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Home = () => {
+  const { message, setMessage } = useMessage();
+
+  useEffect(() => {
+    if (message != '') {
+      toast.success(message);
+      setMessage('')
+    }
+  }, [])
+
   return (
     <>
       <SectionProduct/>
       <Slider/>
+      <ToastContainer position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark" />
+
     </>
     
   )
