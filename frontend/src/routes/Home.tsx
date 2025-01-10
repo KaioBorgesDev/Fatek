66f39d081d19905c5c67346079443cccb89592b7
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-10 15:12:37 -0300

    fix- value toast container

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
       <SectionProduct/>
       <Slider/>
       <ToastContainer position="top-right"
-        autoClose={5000}
+        autoClose={1000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick={false}
