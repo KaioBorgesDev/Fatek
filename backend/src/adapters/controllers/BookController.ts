4ba005e6ae2d7051b27f7f2e7a7f3a74be99bc5f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-14 10:33:26 -0300

    fix- reorder the code

--- backend/src/adapters/controllers/BookController.ts
+++ backend/src/adapters/controllers/BookController.ts
@@ -4,11 +4,9 @@ import BookRepositoryImp from "src/infra/Service/BookRepositoryImp";
 const postBookController = async (req, res) => {
     try {
         const postBook = new PostBookUseCase(new BookRepositoryImp());
-        const id_user = req.body.user.id_user;
-        
         await postBook.execute(req.body); 
-        
         res.status(200).json({ message: "Book created sucessfuly!" });
+
     } catch (error) {
         if (error.code === 11000) {
             return res.status(409).json({
