9c3e8a39ea45c4249cc9fed72bc238ef8dd7b581
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-14 10:12:32 -0300

    fix- try catch when validate token

--- backend/src/adapters/midlewares/AuthJwt.ts
+++ backend/src/adapters/midlewares/AuthJwt.ts
@@ -5,16 +5,23 @@ const secretKey = "SAD";
 
 const authJwt = async (req,res,next) => {
     const validateToken = new ValidateTokenCase(new JwtTokenService(secretKey));
-
     const token = req.headers['authorization']?.split(' ')[1];
 
     if(!token){
-       return res.status(403).json({Error: "Token not provided"});
+        return res.status(403).json({Error: "Token not provided"});
+    }
+
+    try{
+        const payload = await validateToken.execute(token);
+        req.body.user = payload;
+        next();
+    }catch(error){
+        res.status(401).json({
+            "error": "TokenExpiredError",
+            "message": "O token fornecido expirou. Faça login novamente."
+          })
     }
     
-    const payload = await validateToken.execute(token);
-    req.body.user = payload;
-    next();
 }
 
 module.exports = {authJwt};
