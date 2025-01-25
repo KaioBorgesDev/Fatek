4c5a8a1669421c025fd501fc27d53537369d9d0d
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-25 13:43:16 -0300

    feat- user repository with methods for find by id + saveAdress

--- backend/src/adapters/repository/UserRepository.ts
+++ backend/src/adapters/repository/UserRepository.ts
@@ -1,6 +1,9 @@
+import AdressUser from "src/entities/AdressUser";
 import User from "src/entities/User";
 
 export default interface UserRepository{
     save(email: string, password: string): Promise<void>;
     findByEmail(email: string): Promise<User | null>;
+    findById(userId: string): Promise<any>;
+    saveAdress(userId: string, address: AdressUser): Promise<void>;
 }
