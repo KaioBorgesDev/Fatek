aa3126337f0ce0d07ad2913b80fe3f669089b81a
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-25 13:43:47 -0300

    fix - Adress identation + some wrong words

--- backend/src/entities/AdressUser.ts
+++ backend/src/entities/AdressUser.ts
@@ -1,19 +1,31 @@
-export default class AdressUser{
+export default class AdressUser {
+    constructor(
+        public cep: string,
+        public casa: string,
+        public bairro: string,
+        public endereco: string,
+        public cidade: string,
+        public estado: string
+    ) {}
 
-    constructor(public cep: string, public casa: string, public bairro: string, public endereco: string, public cidade: string, public estado: string){
-    }
-
-    static createAdress(cep: string, casa: string, bairro: string, endereco: string, cidade: string, estado: string){
-        if(cep.length > 9){
-            throw new Error("Cep não pode ter mais de 9 digitos");
+    static createAdress(
+        cep: string,
+        casa: string,
+        bairro: string,
+        endereco: string,
+        cidade: string,
+        estado: string
+    ) {
+        if (cep.length > 9) {
+            throw new Error("CEP não pode ter mais de 9 dígitos.");
         }
-        // ve se a casa possui apenas numeros
-        if(!(/^\d+$/.test(casa.trim()))){
-            throw new Error("Casa não pode ter letras.")
+
+        if (!/^\d+$/.test(casa.trim())) {
+            throw new Error("Casa deve conter apenas números.");
         }
 
-        if((/^\d+$/.test(endereco.trim()))){
-            throw new Error("Endereço não pode ser apenas numeros.")
+        if (/^\d+$/.test(endereco.trim())) {
+            throw new Error("Endereço não pode conter apenas números.");
         }
 
         return new AdressUser(cep, casa, bairro, endereco, cidade, estado);
