27ce430c7eaac918a42810853b1e13ae76aa62c3
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-25 13:44:21 -0300

    feat- save adress implemented

--- backend/src/infra/Service/UserRepositoryImp.ts
+++ backend/src/infra/Service/UserRepositoryImp.ts
@@ -1,9 +1,16 @@
 import UserRepository from "src/adapters/repository/UserRepository";
 import User from "src/entities/User";
 import UserModel from "../Model/UserModel";
+import AdressUser from "src/entities/AdressUser";
 
 
 export default class UserRepositoryImp implements UserRepository{
+    async findById(userId: string): Promise<User | null> {
+        return await UserModel.findOne({ id_user: userId });
+    }
+    async saveAdress(userId: string, address: AdressUser): Promise<void> {
+        await UserModel.updateOne({id_user: userId}, { address: { number: address.casa, street: address.endereco, city: address.cidade, state: address.estado, postalCode: address.cep, neighborhood: address.bairro} });
+    }
     async findByEmail(email: string): Promise<User | null> {
         return await UserModel.findOne({email: email})
     }
