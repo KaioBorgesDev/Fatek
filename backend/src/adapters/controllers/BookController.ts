8ec9163fc94ab744e7043c78b8fe268beb15ef3f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-01-26 17:19:01 -0300

    feat- book controller supporting files (image)

--- backend/src/adapters/controllers/BookController.ts
+++ backend/src/adapters/controllers/BookController.ts
@@ -1,11 +1,34 @@
 import PostBookUseCase from "src/usecases/UserCases/PostBookUseCase";
 import BookRepositoryImp from "src/infra/Service/BookRepositoryImp";
+const { v4: uuidv4 } = require('uuid');
+const s3 = require('../../infra/Config/s3');
+
+
+
 
 const postBookController = async (req, res) => {
     try {
+        const file = req.file;
+
+        if(!file){
+            return res.status(400).json({ error: "Imagem do livro é obrigatória"});
+        }
+
+        const fileKey = `books/${uuidv4()}_${file.originalname}}`;
+        
+        const result = await s3.upload({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: fileKey,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+        }).promise();
+
+        req.body.image = result.Location;
+        
         const postBook = new PostBookUseCase(new BookRepositoryImp());
         await postBook.execute(req.body); 
-        res.status(200).json({ message: "Book created sucessfuly!" });
+
+        res.status(201).json({ message: "Book created sucessfuly!" });
 
     } catch (error) {
         if (error.code === 11000) {
