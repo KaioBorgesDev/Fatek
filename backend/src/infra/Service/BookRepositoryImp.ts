1262d206c7b13ea0ac85d9dcdd13d4667bda3752
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-02-25 14:12:09 -0300

    Book repository implements for mysql

--- backend/src/infra/Service/BookRepositoryImp.ts
+++ backend/src/infra/Service/BookRepositoryImp.ts
@@ -1,13 +1,31 @@
 import BookRepository from "src/adapters/repository/BookRepository";
-import BookModel from "../Model/BookModel";
-import UserModel from "../Model/UserModel";
+import { pool } from "../Database/mysql";
 
-export default class BookRepositoryImp implements BookRepository{
+export class MySQLBookRepository implements BookRepository {
 
     async save(book: TypeBook): Promise<void> {
-        
-        if(!await UserModel.findOne({id_user: book.id_user}))
-            throw new Error("User dont exits.");
-        await BookModel.create(book);
+        // Verifica se o usuário existe
+        const queryUser = "SELECT * FROM users WHERE id_user = ?";
+        const [userRows]: any = await pool.execute(queryUser, [book.id_user]);
+
+        if (userRows.length === 0) {
+            throw new Error("User doesn't exist.");
+        }
+
+        // Se o usuário existir, salva o livro
+        const query = `
+            INSERT INTO books (id_user, title, autor, publisher, release_date, category, price, imagem_url)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+        await pool.execute(query, [
+            book.id_user,
+            book.title,
+            book.autor,
+            book.publisher,
+            book.release_date,
+            book.category,
+            book.price,
+            book.imagem_url
+        ]);
     }
 }
