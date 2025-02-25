60f6250026b74d85136a1bd70f0dbd8bc22e2a38
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-02-25 14:15:23 -0300

    type of S3Config

--- backend/src/infra/Config/s3.ts
+++ backend/src/infra/Config/s3.ts
@@ -1,10 +1,16 @@
-const AWS = require('aws-sdk');
+import AWS from 'aws-sdk';
 
+
+interface S3Config {
+  accessKeyId: string;
+  secretAccessKey: string;
+  region: string;
+}
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  region: process.env.AWS_REGION as string
 });
 
-module.exports = s3;
+export default s3;
