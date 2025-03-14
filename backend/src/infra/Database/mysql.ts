aefcfbb30110232d5ed5f2f0fd46b250f73b93bb
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-03-14 16:21:52 -0300

    docker compose uml

--- backend/src/infra/Database/mysql.ts
+++ backend/src/infra/Database/mysql.ts
@@ -1,11 +1,25 @@
 import mysql from "mysql2/promise";
 
-export const pool = mysql.createPool({
+const connectDB = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "senha",
-    database: process.env.DB_NAME || "meu_banco",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "fatek",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
+
+async function testConnection() {
+    try {
+        const connection = await connectDB.getConnection();
+        console.log("✅ Conectado ao banco de dados");
+        connection.release();
+    } catch (err) {
+        console.error("❌ Erro ao conectar ao banco de dados:", err);
+    }
+}
+
+testConnection();
+
+export default connectDB
