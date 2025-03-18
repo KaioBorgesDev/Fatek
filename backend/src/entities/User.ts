8fc006be49174924f677a50e95a17a2e4b74c451
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-03-18 15:25:11 -0300

    feat- validation + name of entitie user

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export default class User {
-  constructor(public email: string, public passwordHash: string, public id_user?: string) {}
+  constructor(public email: string, public passwordHash: string, public id_user?: string, public name?: string) {}
 
   validatePassword(password: string): boolean {
     const hashedPassword = User.hashPassword(password);
@@ -9,12 +9,17 @@ export default class User {
   }
 
 
-  static create(email: string, password: string): User {
+  static create(email: string, password: string, name: string): User {
+    if(password.length < 3) throw new Error('Password must be at least 6 characters long');
+    if(name.length < 3) throw new Error('Name must be at least 3 characters long');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) throw new Error('Invalid email format');
+    const id_user = crypto.randomUUID()
     const passwordHash = this.hashPassword(password);
-    return new User(email, passwordHash);
+    return new User(name, email, passwordHash, id_user);
   }
 
-  
+
   private static hashPassword(password: string): string {
     return crypto.createHash('sha512').update(password).digest('hex');
   }
