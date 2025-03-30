2003f102499305cad07d3920cb8ab8f7e6df41d4
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-03-29 21:48:36 -0300

    feat- CartCard redirect to Checkout

--- frontend/src/components/CartCard/CartCard.tsx
+++ frontend/src/components/CartCard/CartCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoClose } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 
 interface CartCardProps {
     cartActive: boolean;
@@ -7,20 +8,24 @@ interface CartCardProps {
     listProdutos: {name: string, price: number}[];
 }
 const CartCard : React.FC<CartCardProps>= ({cartActive, toggleCart, listProdutos}) => {
+    const navigate = useNavigate();
   return (
-    <div>{/* Cart Card */}
-    <div className={`cart-card ${cartActive ? "active" : ""}`}>
-      <div className="card-head">
-      <h2>Meu Carrinho</h2>
-      <button onClick={toggleCart} className="close-cart"><IoClose size={20}/></button>
-      </div>
-      <ul>
-        {listProdutos.map((produto, index) => (
-          <li key={index}>{produto.name} - R${produto.price.toFixed(2)}</li>
-        ))}
-      </ul>
-      
-    </div></div>
+    <div>
+        <div className={`cart-card ${cartActive ? "active" : ""}`}>
+             <div className="card-head">
+                <h2>Meu Carrinho</h2>
+                <button onClick={toggleCart} className="close-cart"><IoClose size={20}/></button>
+            </div>
+            <ul>
+                {listProdutos.map((produto, index) => (
+                 <li key={index}>{produto.name} - R${produto.price.toFixed(2)}</li>
+                ))}
+            </ul>
+            <div className="card-footer">
+                <button className="btn btn-primary" onClick={()=> navigate('/checkout')}>Finalizar Compra</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
